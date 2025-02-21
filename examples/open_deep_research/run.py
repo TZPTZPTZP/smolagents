58dde47c25e4639492f16de74b2172353e23c470
TZP <tzp@TZPdebijibendiannao.local>
2025-02-21 17:51:55 +0800

    加入token

--- examples/open_deep_research/run.py
+++ examples/open_deep_research/run.py
@@ -141,6 +141,66 @@ def main():
 
     print(f"Got this answer: {answer}")
 
+def local_debug(model_id,question):
+    # args = parse_args()
+    text_limit = 100000
+
+    model = LiteLLMModel(
+        model_id,
+        api_base="https://api-gateway.glm.ai/v1",
+        api_key="sk-8xSiNEEPhMTPSHzlBXFYezb1J1FEWR4R",
+        custom_role_conversions=custom_role_conversions,
+        max_completion_tokens=8192,
+        reasoning_effort="high",
+    )
+    document_inspection_tool = TextInspectorTool(model, text_limit)
+
+    browser = SimpleTextBrowser(**BROWSER_CONFIG)
+
+    WEB_TOOLS = [
+        SearchInformationTool(browser),
+        VisitTool(browser),
+        PageUpTool(browser),
+        PageDownTool(browser),
+        FinderTool(browser),
+        FindNextTool(browser),
+        ArchiveSearchTool(browser),
+        TextInspectorTool(model, text_limit),
+    ]
+
+    text_webbrowser_agent = ToolCallingAgent(
+        model=model,
+        tools=WEB_TOOLS,
+        max_steps=20,
+        verbosity_level=2,
+        planning_interval=4,
+        name="search_agent",
+        description="""A team member that will search the internet to answer your question.
+    Ask him for all your questions that require browsing the web.
+    Provide him as much context as possible, in particular if you need to search on a specific timeframe!
+    And don't hesitate to provide him with a complex search task, like finding a difference between two webpages.
+    Your request must be a real sentence, not a google search! Like "Find me this information (...)" rather than a few keywords.
+    """,
+        provide_run_summary=True,
+    )
+    text_webbrowser_agent.prompt_templates["managed_agent"]["task"] += """You can navigate to .txt online files.
+    If a non-html page is in another format, especially .pdf or a Youtube video, use tool 'inspect_file_as_text' to inspect it.
+    Additionally, if after some searching you find out that you need more information to answer the question, you can use `final_answer` with your request for clarification as argument to request for more information."""
+
+    manager_agent = CodeAgent(
+        model=model,
+        tools=[visualizer, document_inspection_tool],
+        max_steps=12,
+        verbosity_level=2,
+        additional_authorized_imports=AUTHORIZED_IMPORTS,
+        planning_interval=4,
+        managed_agents=[text_webbrowser_agent],
+    )
+
+    answer = manager_agent.run(question)
+
+    print(f"Got this answer: {answer}")
 
 if __name__ == "__main__":
-    main()
+    # main()
+    local_debug("gpt-4o-2024-11-20", "What is the capital of France?")
